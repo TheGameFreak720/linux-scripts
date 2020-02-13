5aae1d1bed56d5d7b30cc0ed5c697907a5744279
Luis Alvarez <luis.alvarez@commithub.com>
2020-02-13 15:27:24 -0500

    added new script to install the fedora env. Not sure if it will work entirely but after testing each command I will know

---
+++ set-dev-env-fedora.sh
@@ -0,0 +1,133 @@
+ #!/bin/bash
+
+#For Fedora Linux
+
+#NodeJS
+sudo dnf install nodejs
+
+#Yarn
+sudo dnf install yarn
+
+#Vim
+sudo dnf install vim-enhanced
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/myusuf3/numbers.vim.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/Badacadabra/vim-archery.git
+git clone https://github.com/preservim/nerdtree.git
+git clone https://github.com/heavenshell/vim-jsdoc.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/kchmck/vim-coffee-script.git
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/lepture/vim-css.git
+git clone https://github.com/posva/vim-vue.git
+
+cd ~
+cat > .vimrc <<- "EOF"
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+" On pressing tab, insert 2 spaces
+set expandtab
+" show existing tab with 2 spaces width
+set tabstop=2
+set softtabstop=2
+" when indenting with '>', use 2 spaces width
+set shiftwidth=2
+set autoindent
+set clipboard=unnamedplus "yank across terminals
+autocmd BufWritePre * :%s/\s\+$//e " remove whitespaces on save
+
+nnoremap <F3> :NumbersToggle<CR>
+nnoremap <F4> :NumbersOnOff<CR>
+
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0
+
+colorscheme archery
+EOF
+
+#CLI
+npm install -g @vue/cli gatsby-cli create-react-app eslint
+curl https://cli-assets.heroku.com/install.sh | sh
+
+#Spotify
+sudo dnf install lpf-spotify-client
+
+#Arduino
+sudo dnf install arduino
+
+#Postman
+wget https://dl.pstmn.io/download/latest/linux64 -O postman-linux-x64.tar.gz
+sudo tar xvzf postman-linux-x64.tar.gz -C /opt
+sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+cat << EOF > ~/.local/share/applications/postman2.desktop
+[Desktop Entry]
+Name=Postman
+GenericName=API Client
+X-GNOME-FullName=Postman API Client
+Comment=Make and view REST API calls and responses
+Keywords=api;
+Exec=/opt/Postman/Postman
+Terminal=false
+Type=Application
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Categories=Development;Utilities;
+EOF
+
+#Git
+sudo dnf install git-all
+
+#Tmux
+sudo dnf -y install tmux
+cd ~
+cat > .tmux.conf <<- "EOF"
+# Send Prefix
+set-option -g prefix C-a
+unbind-key C-a
+bind-key C-a send-prefix
+
+# Use Alt-arrow keys to switch panes
+bind -n M-Left select-pane -L
+bind -n M-Right select-pane -R
+bind -n M-Up select-pane -U
+bind -n M-Down select-pane -D
+
+# Shift arrow to switch windows
+bind -n S-Left previous-window
+bind -n S-Right next-window
+
+# Mouse Mode
+set -g mouse on
+
+# Set easier window split keys
+bind-key v split-window -h
+bind-key h split-window -v
+
+# Copy Paste
+bind P paste-buffer
+bind-key -T copy-mode-vi v send-keys -X begin-selection
+bind-key -T copy-mode-vi y send-keys -X copy-selection
+bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
+EOF
+
+#htop
+sudo dnf install htop
+
+#Slack
+sudo dnf -y install wget
+wget https://downloads.slack-edge.com/linux_releases/slack-3.4.0-0.1.fc21.x86_64.rpm
+sudo dnf localinstall slack-3.4.0-0.1.fc21.x86_64.rpm
+
+
