f3a749ecd0c41a39b80b8fb48d7bfc2b5efb91d2
Ori Alvarez <ori.alvarez@commithub.com>
2020-11-09 11:41:12 -0500

    Update set-dev-env-fedora.sh

--- set-dev-env-fedora.sh
+++ set-dev-env-fedora.sh
@@ -51,6 +51,8 @@ set statusline+=%#warningmsg#
 set statusline+=%{SyntasticStatuslineFlag()}
 set statusline+=%*
 
+:set colorcolumn=100
+
 let g:syntastic_always_populate_loc_list = 1
 let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
